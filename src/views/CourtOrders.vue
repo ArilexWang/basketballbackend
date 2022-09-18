0e702d294468545f3fb37b79362b7a73255cb823
Alex <383234436@qq.com>
2022-09-18 17:27:47 +0800

    fix:3.3

--- src/views/CourtOrders.vue
+++ src/views/CourtOrders.vue
@@ -366,7 +366,7 @@ export default {
             this.$prompt("请输入密码", "提示", {
                 confirmButtonText: "确定",
                 cancelButtonText: "取消",
-                inputType: "password"
+                inputType: "new-password"
             })
                 .then(({ value }) => {
                     if (value !== "947117") {
--- src/views/vip/vipCourt.vue
+++ src/views/vip/vipCourt.vue
@@ -312,7 +312,7 @@ export default {
             this.$prompt("请输入密码", "提示", {
                 confirmButtonText: "确定",
                 cancelButtonText: "取消",
-                inputType: "password"
+                inputType: "new-password"
             }).then(({ value }) => {
                 if (value !== "947117") {
                     this.$message({
